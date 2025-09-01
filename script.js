fa817a17636690ca44ff50796b509acec0fd57f7
Enzo. <enzo.ferreira.carvalho@escola.pr.gov.br>
2025-09-01 08:01:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
+const calcular = document.getElementById('calcular');
+
 function imc(){
-    const nome = document.getElementById('nome').value
-    const altura = document.getElementById('altura').value
-    const peso = document.getElementById('peso').value
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const resultado = document.getElementById
+    ('resultado');
+    
+
+    if(nome != "" && altura !="" && peso !=""){
+
+        const valorIMC = (peso/(altura*altura)).toFixed(1);
+        let classicacao = "";
+
+        if(valorIMC < 18.5){
+            classificacao = "Abaixo do peso":
+        }
+
+    }
+
+    resultado.textContent = `${nome} seu IMC é ${valorIMC}!`;
 }
+
+calcular.addEventListener('click',imc);
